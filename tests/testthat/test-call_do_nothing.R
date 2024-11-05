c368fa07cecb97eaa97c3858645ada3d4eadbced
Mauro Lepore <maurolepore@gmail.com>
2024-11-05 19:00:39 +0000

    Prune tests

--- tests/testthat/test-call_do_nothing.R
+++ tests/testthat/test-call_do_nothing.R
@@ -1,8 +1,3 @@
 test_that("works with a character", {
   expect_equal(call_do_nothing("a"), "a")
 })
-
-test_that("works with a data frame", {
-  data <- datasets::BOD
-  expect_equal(call_do_nothing(data), data)
-})
